661d2403c6eb2c2cd7c4fb2d7658db0b6875a7c5
David <davidmclaugh@gmail.com>
2020-09-22 23:27:16 -0700

    Add an android build target

---
+++ android/src/com/gdx/cellular/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gdx.cellular;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gdx.cellular.CellularAutomaton;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CellularAutomaton(), config);
+	}
+}
